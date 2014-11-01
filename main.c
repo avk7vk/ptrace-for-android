4277620fa7770873a2dc9560dd9bced5187f6af7
avk7vk <vamsikrishna.atluri@stonybrook.edu>
2014-11-01 18:57:34 -0400

    Basic Ptrace working independent of 64/32 bit processor

--- main.c
+++ main.c
@@ -3,45 +3,50 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <errno.h> /*errno*/
-#include <sys/reg.h>
+#include <errno.h>
+#include <sys/reg.h> /*ORIG_RAX, RAX*/
 
-#define ORIG_EAX 11 /*Include a Header Instead*/
+/* Determine if 64/32 bit */
+#ifdef ORIG_EAX
+	const int const_orig_eax = ORIG_EAX;
+	const int const_eax = EAX;
+#else
+	 const int const_orig_eax = ORIG_RAX;
+	 const int const_eax = RAX;
+#endif
+
+int wait_for_syscall(pid_t child);
 int main()
 {
 	pid_t childPid;
+	long orig_eax, eax;
 	int status;
-	long orig_eax;
 
 	childPid = fork();
 	if(childPid >= 0) {
-		if(childPid == 0) { // In Child Process
-			printf("========Hi I am in Child Process=======\n");
+
+		if(childPid == 0) { /*In Child Process */ 
 			ptrace(PTRACE_TRACEME, 0, NULL, NULL);
 			execl("/bin/ls","ls", NULL);
-
 		}
-		else { // In Parent Process
+		else { /*In Parent Processs */
 			printf("\n========Hi I am in Parent Process=======\n");
-			printf("CHild PID = %d\n", childPid);
-			while(1){
-				
-				wait(&status);//Blocks till child terminates
-				
-				if(WIFEXITED(status)) {
-					printf("Child Process Exited Normally\n");
-					break;
-				}
-				else {
-					printf("Child Process was stopped!\n");
-				}
+			printf("Child PID = %d\n", childPid);
 
-				//printf("Parent: Ahh! my wait is over!\n");
+			waitpid(childPid, &status, 0);
+			ptrace(PTRACE_SETOPTIONS, childPid, 0, 
+				PTRACE_O_TRACESYSGOOD);
+			
+			while(1){
+				if(wait_for_syscall(childPid) != 0) break;
 				orig_eax = ptrace(PTRACE_PEEKUSER, childPid, 
-					4 * ORIG_EAX, NULL);
-				printf("System Call Number is %ld\n", orig_eax);
-				printf("Error is %d\n", errno);
-				ptrace(PTRACE_SYSCALL, childPid, NULL, NULL);
+						sizeof(long) * const_orig_eax, NULL);
+				printf("System Call Number is %ld ", orig_eax);
+				
+				if(wait_for_syscall(childPid) != 0 ) break;
+				eax = ptrace(PTRACE_PEEKUSER, childPid, 
+					sizeof(long)*const_eax, NULL);
+				printf("Return Value = %ld\n",eax);
 			}
 		}
 	}
@@ -51,4 +56,19 @@ int main()
 	}
 
 	return 0;
+}
+
+int wait_for_syscall(pid_t child) {
+	int status;
+	while(1) {	
+		ptrace(PTRACE_SYSCALL, child, NULL, NULL);	
+		waitpid(child, &status, 0);
+		if (WIFSTOPPED(status) && (WSTOPSIG(status) & 0x80)) {
+			return 0;
+		}
+		if (WIFEXITED(status)) {
+			printf("Child process exited Normally\n");
+			return 1;
+		}
+	}
 }
